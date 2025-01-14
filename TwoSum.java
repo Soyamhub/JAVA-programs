82c406e0acc1652daba9ce57b32d5a1ef9a79401
Soyamhub <soyamshubhamswain@gmail.com>
2025-01-14 20:58:32 +0530

    Changing the two sum solution

--- TwoSum.java
+++ TwoSum.java
@@ -1,7 +1,10 @@
-import java.util.*;;
+
+import java.util.Arrays;
+
 
 public class TwoSum {
     public static void main(String[] args) {
+        /* 
         int[] nums = {3,2,4};
         int target = 6;
         int[] result = new int[2];
@@ -13,8 +16,19 @@ public class TwoSum {
                 if(remain == nums[j]){
                     result[1] = j;
                     System.out.println(Arrays.toString(result));
+                    
                 }
             }
         } 
+       */
+        int[] nums = {3,2,4};
+        int target = 6;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    System.out.println(Arrays.toString(new int[]{i,j}));
+                }
+            }
+        }     
     }
 }
