de8c8f9c956c93ef269246e6ccbef01371aacb80
Soyamhub <soyamshubhamswain@gmail.com>
2025-01-16 20:43:46 +0530

    Adding Binary search problem

--- ArrayProblems1.java
+++ ArrayProblems1.java
@@ -1,11 +1,10 @@
 
-import java.lang.reflect.Array;
 
 public class ArrayProblems1 {
     public static void main(String[] args) {
         int[] arr = {5,3,20,9,3};
         MaxValue(arr);
-        
+        SearchElement(arr, 20);
         
     }
     static void MaxValue(int[] arr){
@@ -18,5 +17,21 @@ public class ArrayProblems1 {
         }
         System.out.println("The max value is "+ max);
         
+    }
+    static void SearchElement(int[] arr, int x){
+        //Search the given number in the array(Binary search) and return the index
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]) {
+                index = i;
+                break;
+            }
+        }
+        if(index == 0){
+            System.out.println("Not found.");
+        }
+        else{
+            System.out.println("The value is found at "+ index);
+        }
     }
 }
