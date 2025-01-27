39e96ef16da612cb59c2072afb6bc9fad3d4b640
Gurpreet Kaur <155377816+gurpreetk3012@users.noreply.github.com>
2025-01-27 22:24:32 +0530

    added some more pattern programs into the repository

--- Pattern programs/NumberPattern1.java
+++ Pattern programs/NumberPattern1.java
@@ -1,24 +1,21 @@
-public class Pattern2 {
-    
+public class NumberPattern1 {
+
     /*
-    
     Output- 
-        *****
-        ****
-        ***
-        **
-        *
-
+        1
+        12
+        123
+        1234
+        12345
      */
-
     public static void main(String[] args) {
         
         int row, column;
-        for (row = 5; row>=1; row--)
+        for (row = 1; row<=5; row++)
         {
             for (column = 1; column<=row; column++)
             {
-                System.out.print("*");
+                System.out.print(column);
             }
             System.out.println();
         }
--- Pattern programs/NumberPattern3.java
+++ Pattern programs/NumberPattern3.java
@@ -1,22 +1,21 @@
-public class Pattern1 {
+public class NumberPattern3 {
     
-    /*
+    /*  
     Output- 
-            *
-            **
-            ***
-            ****
-            *****
-     */
-
+        54321
+        5432
+        543
+        54
+        5
+    */
     public static void main(String[] args) {
         
         int row, column;
         for (row = 1; row<=5; row++)
         {
-            for (column = 1; column<=row; column++)
+            for (column = 5; column>=row; column--)
             {
-                System.out.print("*");
+                System.out.print(column);
             }
             System.out.println();
         }
